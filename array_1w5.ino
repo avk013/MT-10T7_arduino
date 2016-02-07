6ac1a303c757e54b8b7f8aa53e228880a13accfd
Ali Koba <avk013@gmail.com>
2016-02-07 09:55:43 +0200

    Update array_1w5.ino
    добавлена возможность выводить отрицательные нецелые числа, вместо только действительных прошлой версии

--- array_1w5.ino
+++ array_1w5.ino
@@ -1,9 +1,9 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#define ONE_WIRE_BUS_1 7
+#define ONE_WIRE_BUS_1 2
 //////////////////////
 #define PIN_WR1 3
-#define PIN_WR2 2
+//#define PIN_WR2 2
 #define PIN_A0 13
 #define PIN_DB0 9
 #define PIN_DB1 10
@@ -18,8 +18,8 @@
 #define SEG_G 1
 #define SEG_H 16
 //////////////////////////////
-static byte num[10] = {
-  SEG_F | SEG_E | SEG_D | SEG_A | SEG_B | SEG_C ,
+static byte num[11] = {
+  SEG_F | SEG_E | SEG_D | SEG_A | SEG_B | SEG_C,
   SEG_B | SEG_C ,
   SEG_E | SEG_D | SEG_A | SEG_B | SEG_G,
   SEG_D | SEG_A | SEG_B | SEG_C | SEG_G,
@@ -29,7 +29,7 @@ static byte num[10] = {
   SEG_A | SEG_B | SEG_C ,
   SEG_F | SEG_E | SEG_D | SEG_A | SEG_B | SEG_C | SEG_G,
   SEG_F | SEG_D | SEG_A | SEG_B | SEG_C | SEG_G
-  };
+    };
 static byte STR_HELLO[5] = {
   SEG_F | SEG_E | SEG_G | SEG_B | SEG_C,
   SEG_A | SEG_F | SEG_G | SEG_E | SEG_D,
@@ -46,17 +46,19 @@ void setup() {
     sensor_inhouse.begin();
 ////////
 delay(3000);//обнуление
-pinMode(PIN_A0, OUTPUT); pinMode(PIN_WR1, OUTPUT);pinMode(PIN_WR2, OUTPUT);
+pinMode(PIN_A0, OUTPUT); pinMode(PIN_WR1, OUTPUT);//pinMode(PIN_WR2, OUTPUT);
 pinMode(PIN_DB0, OUTPUT);pinMode(PIN_DB1, OUTPUT);
 pinMode(PIN_DB2, OUTPUT);pinMode(PIN_DB3, OUTPUT);
-digitalWrite(PIN_WR1, LOW);digitalWrite(PIN_WR2, LOW);}
+digitalWrite(PIN_WR1, LOW);//digitalWrite(PIN_WR2, LOW);
+}
 void _write_dbs(byte nibble) { //запись битов младших 4 разрядов
   digitalWrite(PIN_DB0, nibble & 1 ? HIGH : LOW);
   digitalWrite(PIN_DB1, nibble & 2 ? HIGH : LOW);
   digitalWrite(PIN_DB2, nibble & 4 ? HIGH : LOW);
   digitalWrite(PIN_DB3, nibble & 8 ? HIGH : LOW);}
 void _write_wr() { // переключиние на запись
-  digitalWrite(PIN_WR2, LOW); digitalWrite(PIN_WR1, HIGH);
+  //digitalWrite(PIN_WR2, LOW); 
+  digitalWrite(PIN_WR1, HIGH);
   delayMicroseconds(5);digitalWrite(PIN_WR1, LOW);}
 void write_lcd_raw(byte addr, byte data) {
   _write_dbs(addr & 15);
@@ -68,18 +70,22 @@ void write_lcd_raw(byte addr, byte data) {
   // Unnecessary cleanup
   digitalWrite(PIN_A0, LOW); _write_dbs(0); }
 //
-void go_display(byte number, byte razr) {
+void go_display(float number, byte razr) {
+byte point=0,zap=0, out;
 String stroka=String(number);
-Serial.print(stroka);
-Serial.print('_');
 char *z= new char[stroka.length()+1];
 stroka.toCharArray(z,stroka.length()+1);
 for (int i = 0; i<stroka.length(); i++){
 int b=z[i]-'0';
-write_lcd_raw(razr+i, num[b]);
-delay(100);
-//Serial.print(b);
-//Serial.print('+');
+out=num[b];
+//if (z[i+1]-'0'==-2 && b!=-3) point=16;
+if (z[i+1]-'0'==-2) point=16;
+if (b==-3) out=1;
+write_lcd_raw(razr+i-zap, out+point);
+if (point==16) {point=0;i++;zap=1;}
+delay(5);
+Serial.print(b);
+Serial.print('+');
 }
 delete[] z, stroka;  };
 //////////////////
@@ -96,34 +102,19 @@ void loop() {
 for (int i = 0; i < 10; i++) {
     write_lcd_raw(i, 0);
   }
-  delay(500);
+  delay(50);
   for (int i = 0; i < 10; i++) {
-    write_lcd_raw(i, SEG_G);
-    delay(100);
-  }
-  delay(1000);
+    write_lcd_raw(i, SEG_G); delay(50);}
+  delay(500);
   for (int i = 0; i < 10; i++) { write_lcd_raw(i, 0); }  
-
-  int offset = 3;
-  int dir = 1, nom=1, op;
-  //String stroka=String(sensor_inhouse.getTempCByIndex(0),DEC);
-  //String stroka;
+  int dir = 1,offset = 1;
+  float temp=5;
   while (1) {
-/*stroka=String(dir++);
-char *z= new char[stroka.length()+1];
-stroka.toCharArray(z,stroka.length()+1);
-for (int i = 0; i<stroka.length(); i++){
-int b=z[i]-'0';
-write_lcd_raw(i, num[b]);*/
-go_display(dir++,2);
-delay(100);
-//Serial.print(b);
-//Serial.print('+');
-//}
-//delete[] z;
+temp-=0.01;
+go_display(temp,offset);
 Serial.print('=');
-Serial.print(dir);
+Serial.print(temp);
 Serial.println();
-delay(50);
+delay(50);  
   }
 }
